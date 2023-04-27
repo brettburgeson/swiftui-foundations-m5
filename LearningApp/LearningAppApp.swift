ab8a3885dd2d867b46ce5ebac6baf35f31b3d88c
Brett <brettb4home@gmail.com>
2023-04-26 19:44:10 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Brett Burgeson on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
